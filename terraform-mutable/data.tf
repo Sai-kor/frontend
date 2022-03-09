b2ae452231331375c92965fb49599a826fe4e31f
sai k <psai0073@gmail.com>
2022-03-09 02:37:55 -0500

    ami data.tf commented one line

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "ami" {
-  executable_users = ["self"]
+#  executable_users = ["self"]
   most_recent      = true
   name_regex       = "base-with-ansible"
   owners           = ["self"]
