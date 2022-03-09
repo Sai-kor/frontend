0e1053788c74c1d9e83155c72654adcdbc48f0f7
sai k <psai0073@gmail.com>
2022-03-09 11:18:45 -0500

    sg port 80 fro instances

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -8,6 +8,7 @@ resource "aws_spot_instance_request" "ec2-spot" {
  subnet_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS[count.index]
   #creates ec2 spot instance in that particular vpc and private subnet id getting data from data.tf remote state resource
   wait_for_fulfillment = true
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
 resource "aws_ec2_tag" "spot-instances" {
