825fc4eaeab957e6049b6ada5cda8a104a0a9bba
sai k <psai0073@gmail.com>
2022-03-09 18:06:24 -0500

    roboshop frotend component deploy on instance app-deploy setup secrets.

---
+++ terraform-mutable/app-deploy.tf
@@ -0,0 +1,15 @@
+## this null resource do nothing , but we give provisioner , provisioner is one which is going to help you to connect to ec2 instances/linux instances by providing required information.It can connect to the instances and execute the commands
+resource "null_reosurce" "app-deploy" {
+  count = length(aws_spot_instance_request.ec2-spot)
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
+      host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
+    }
+    inline = [
+      "ansible-pull -U https://github.com/Sai-kor/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+    ]
+  }
+}
--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -24,3 +24,11 @@ data "terraform_remote_state" "alb" {
   }
 }
 
+data "aws_secretsmanager_secret" "password" {
+  name = "pswd"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.password.id
+}
+
