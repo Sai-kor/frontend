330df8aa4b3fe839d8c665924818ec2fcbfe71a0
sai k <psai0073@gmail.com>
2022-03-09 19:43:28 -0500

    app version command line input setup

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -9,7 +9,7 @@ resource "null_resource" "app-deploy" {
   }
   provisioner "remote-exec" {
     inline = [
-      "ansible-pull -U https://github.com/Sai-kor/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+      "ansible-pull -U https://github.com/Sai-kor/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION}"
     ]
   }
 }
